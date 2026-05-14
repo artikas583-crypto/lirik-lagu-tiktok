22e6b0d399053d254dd76f380f70bdc6a247d38f
artikas583-crypto <artikas583@gmail.com>
2026-05-14 07:04:18 +0700

    Update lirik.py

--- lirik.py
+++ lirik.py
@@ -5,19 +5,20 @@ import time
 def jalanin_lirik():
     # Ubah lirik lagu dan delay hurufnya sesuai yang kalian mau
     lirik = [
-        ("Dengar Laraku", 0.1),
-        ("Suara hati ini memanggil namamu", 0.09),
-        ("Karena separuh aku", 0.09),
-        ("Menyentuh laramu", 0.09),
-        ("Semua lukamu t'lah menjadi lirihku", 0.09),
-        ("Kar'na separuh aku", 0.09),
-        ("Dirimu", 0.09),
+        ("I’m lookin’ back on things I’ve done
+", 0.1),
+        ("I never wanna play the same old part", 0.09),
+        ("Keep you in the dark", 0.09),
+        ("Now let me show you the shape of my heart", 0.09),
+        ("Looking back on the things I’ve done", 0.09),
+        ("I was trying to be someone", 0.09),
+        ("Played my part, kept you in the dark", 0.09),
     ]
 
     # Ubah delay dari setiap baris lagu (sesuaikan jumlah)
     delay = [0.3, 0.2, 0.3, 0.4, 0.6, 0.3, 0.3]
     # Ubah judul lagu
-    print("\n== Separuh Aku - Noah ==")
+    print("\n== Shape of My Heart - Backstreet Boys ==")
     for i, (baris_lagu, delay_karakter) in enumerate(lirik):
         for karakter in baris_lagu:
             print(karakter, end='')
